84e6863414b0e7c39b214c037fbdbb43fe1d31fc
Supriya Reddy <121212953+Supriya-wl@users.noreply.github.com>
2023-03-20 12:15:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
 //your code here
+let draggableItem = null;
+
+// Add event listener to all the draggable items
+document.querySelectorAll('.draggable').forEach(item => {
+  item.addEventListener('dragstart', dragStart);
+  item.addEventListener('dragover', dragOver);
+  item.addEventListener('dragenter', dragEnter);
+  item.addEventListener('dragleave', dragLeave);
+  item.addEventListener('drop', drop);
+  item.addEventListener('dragend', dragEnd);
+});
+
+// Drag Start
+function dragStart(e) {
+  draggableItem = e.target;
+  setTimeout(() => {
+    e.target.classList.add('invisible');
+  }, 0);
+}
+
+// Drag Over
+function dragOver(e) {
+  e.preventDefault();
+}
+
+// Drag Enter
+function dragEnter(e) {
+  e.preventDefault();
+  this.classList.add('hovered');
+}
+
+// Drag Leave
+function dragLeave(e) {
+  this.classList.remove('hovered');
+}
+
+// Drop
+function drop(e) {
+  this.classList.remove('hovered');
+  if (draggableItem !== this) {
+    let temp = draggableItem.innerHTML;
+    draggableItem.innerHTML = this.innerHTML;
+    this.innerHTML = temp;
+  }
+}
+
+// Drag End
+function dragEnd(e) {
+  e.target.classList.remove('invisible');
+  draggableItem = null;
+}
